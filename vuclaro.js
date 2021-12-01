82c427f2f0d9aa7f6ddac30ff9a877128354201b
jneylon@mednet.ucla.edu <jpneylon@users.noreply.github.com>
2021-12-01 11:08:59 -0800

    add express server for static production

---
+++ vuclaro.js
@@ -0,0 +1,14 @@
+// wire up the module
+// const express = require('express') 
+import express from 'express'
+// create server instance
+const app = express() 
+// bind the request to an absolute path or relative to the CWD
+app.use(express.static('frontend/dist'))
+// Connection Info
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 3000;
+const host = process.env.EXP_IP || 'localhost';
+// start the server
+app.listen(port, host, () => console.log(`Listening on http://${host}:${port}`))
